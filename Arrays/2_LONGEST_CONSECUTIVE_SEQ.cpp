d12d3e470f5e8b741d61993ccfa964054aea4224
Your Name <you@example.com>
2025-12-23 11:02:46 +0530

    2nd problem solved

--- Arrays/2_LONGEST_CONSECUTIVE_SEQ.cpp
+++ Arrays/2_LONGEST_CONSECUTIVE_SEQ.cpp
@@ -3,7 +3,9 @@ public:
     int longestConsecutive(vector<int>& nums) {
         // brute force solution 
         sort(nums.begin(),nums.end());
+
         int c=1,n=nums.size(),maxi=1;
+        
         if(n<=1) return n;
         for(int i=1;i<n;i++){
             if(nums[i]-nums[i-1]==1){
