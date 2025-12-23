b546a814afa14aa8266bd166c855cc3f80a71a06
Aditya <aditya.2201139ec@iiitbh.ac.in>
2025-12-23 11:38:04 +0530

    2nd problem solved

--- Arrays/2_LONGEST_CONSECUTIVE_SEQ.cpp
+++ Arrays/2_LONGEST_CONSECUTIVE_SEQ.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         // brute force solution 
-        sort(nums.begin(),nums.end());
+        unordered_set<int> st(nums.begin(),nums.end());
 
-        int c=1,n=nums.size(),maxi=1;
-        
-        if(n<=1) return n;
-        for(int i=1;i<n;i++){
-            if(nums[i]-nums[i-1]==1){
-                c++;
+        int ans=0,n=nums.size();
+
+        for(auto num:st){
+            if(!st.count(num-1)){
+                int curr=num;
+                int len=1;
+
+                while(st.count(curr+1)){
+                    len++;
+                    curr++;
+                }
+
+                ans=max(ans,len);
             }
-            else if(nums[i]-nums[i-1]==0){
-                continue;
-            }
-            else{
-                c=1;
-            }
-            
-            maxi=max(maxi,c);
         }
-        return maxi;
+        return ans;
     }
 };
