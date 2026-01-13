b0c284f2633f1c2e6fae6a8ca9d813882ed11ecf
Aditya <aditya.2201139ec@iiitbh.ac.in>
2026-01-13 07:58:44 +0530

    Updated 2_sum

--- Arrays/1_2SUM.cpp
+++ Arrays/1_2SUM.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
+
         unordered_map<int,int> mpp;
+
         for(int i=0;i<n;i++){
             mpp[nums[i]]=i;
         }
 
         for(int i=0;i<n;i++){
-            if(mpp[target-nums[i]] && mpp[target-nums[i]]!=i){
+
+            if(!mpp[target-nums[i]] && mpp[target-nums[i]]!=i){
                 return {i,mpp[target-nums[i]]};
             }
+            
         }
 
         return {0,0};
